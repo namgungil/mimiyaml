4cdb1a456f408019332b35928058b2f6cf52e68e
namgungil <il1358@naver.com>
2025-02-11 20:31:43 +0900

    yaml

---
+++ src/test/java/com/msashop/mimiyaml/MimiyamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msashop.mimiyaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimiyamlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
